f0fc2d075461fe7087d3cce5e5dd85ee151b4eb9
fazliddin <fazliddinxamdamov102@gmail.com>
2022-01-29 16:15:22 +0500

    First Commit

---
+++ src/main/java/com/fazliddin/newwarehouse/NewWarehouseApplication.java
@@ -0,0 +1,13 @@
+package com.fazliddin.newwarehouse;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NewWarehouseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NewWarehouseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/fazliddin/newwarehouse/NewWarehouseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fazliddin.newwarehouse;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NewWarehouseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
